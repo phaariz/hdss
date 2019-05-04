0225ed21f0d7f98648257b1060b0c2ea1f0cadf0
Raakko <rashim900@gmail.com>
2019-05-04 20:45:15 +0530

    fetch number of patients per timeframe

--- src/assets/scripts/functions.js
+++ src/assets/scripts/functions.js
@@ -6,3 +6,12 @@ get('https://dssapi.nodescave.com/diseases').then(function(response) {
     console.error("Failed!", error);
   });
 }
+
+function getNumberOfPatients(days) {
+  get('https://dssapi.nodescave.com/numberofpatients/?param='+days).then(function(response) {
+    var NumberOfPatientsInTime = JSON.parse(response);
+      console.log("Success!", NumberOfPatientsInTime);
+    }, function(error) {
+      console.error("Failed!", error);
+    });
+  }
